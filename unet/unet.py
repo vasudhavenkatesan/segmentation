ef0150d6a4058ddd3b662318f319d6d133514c8d
Vasudha Venkatesan <vasudha0397@gmail.com>
2022-06-08 00:29:30 +0200

    unet model complete

---
+++ unet/unet.py
@@ -0,0 +1,46 @@
+import torch
+import torch.nn as nn
+from unet_modules import *
+
+
+class UNET(nn.Module):
+    def __init__(self, n_channels, n_classes):
+        super(UNET, self).__init__()
+        self.n_channels = n_channels
+        self.n_classes = n_classes
+
+        self.encoder = DoubleConvolution(self.n_channels, 64)
+        self.down1 = Down(64, 128)
+        self.down2 = Down(128, 256)
+        self.down3 = Down(256, 512)
+        self.down4 = Down(512, 1024)
+        self.up1 = Up(1024, 512)
+        self.up2 = Up(512, 256)
+        self.up3 = Up(256, 128)
+        self.up4 = Up(128, 64)
+        self.out = OutConvolution(64, n_classes)
+
+    def forward(self, x):
+        skip1 = self.encoder(x)
+        skip2 = self.down1(skip1)
+        skip3 = self.down2(skip2)
+        skip4 = self.down3(skip3)
+        skip5 = self.down4(skip4)
+        x = self.up1(skip5, skip4)
+        x = self.up2(x, skip3)
+        x = self.up3(x, skip2)
+        x = self.up4(x, skip1)
+        final = self.out(x)
+        return final
+
+
+def test():
+    x = torch.randn((1, 95, 512, 512))
+    model = UNET(n_channels=95, n_classes=3)
+    preds = model(x)
+    print(preds.shape)
+    print(x.shape)
+
+
+if __name__ == '__main__':
+    test()
--- unet/unet_modules.py
+++ unet/unet_modules.py
@@ -1,91 +1,59 @@
 import torch
 import torch.nn as nn
-import torchvision.transforms.functional as TF
+import torch.nn.functional as F
 
 
 class DoubleConvolution(nn.Module):
     def __init__(self, in_channels, out_channels):
-        super(DoubleConvolution, self).__init__()
-        self.double_conv = nn.Sequential(nn.Conv3d(in_channels, out_channels, kernel_size=3, bias=False),
-                                         nn.BatchNorm3d(out_channels),
-                                         nn.ReLU(inplace=True)
-                                         )
-        print(f'in - {in_channels} , out - {out_channels}')
+        super().__init__()
+
+        self.double_conv = nn.Sequential(
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True)
+        )
 
     def forward(self, x):
         return self.double_conv(x)
 
 
-class UNET(nn.Module):
-    def __init__(self, in_channels, out_channels, features=[64, 128, 256, 512]):
-        super(UNET, self).__init__()
-        self.down = nn.ModuleList()
-        self.up = nn.ModuleList()
-        self.pool = nn.MaxPool3d(kernel_size=3)
-
-        # Down of UNET
-        for feature in features:
-            self.down.append(DoubleConvolution(in_channels, feature))
-            in_channels = feature
-
-        # Up of UNET
-        for feature in reversed(features):
-            self.up.append(nn.ConvTranspose3d(feature * 2, feature, kernel_size=3))
-            self.up.append(DoubleConvolution(feature * 2, feature))
-
-        # bottom of UNET
-        self.base = DoubleConvolution(features[-1], features[-1] * 2)
-        self.final_conv = nn.Conv3d(features[-1], out_channels, kernel_size=3)
+class Down(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.maxpool = nn.Sequential(
+            nn.MaxPool2d(2),
+            DoubleConvolution(in_channels, out_channels)
+        )
 
     def forward(self, x):
-        skip_connections = []
-
-        for down in self.down:
-            x = down(x)
-            skip_connections.append(x)
-            x = self.pool(x)
-        print('Finished encoder part')
-        x = self.base(x)
-        print('Before up')
-        skip_connections = skip_connections[::-1]
-
-        for i in range(0, len(self.up), 2):
-            x = self.up[i](x)
-            skip_connection = skip_connections[i // 2]
-            
-            print('after skip connections')
-            if x.shape != skip_connection.shape:
-                x = TF.resize(x, size=skip_connection.shape[1:])
-
-            add_skip = torch.cat((skip_connection, x), dim=1)
-            x = self.up[i + 1].up(add_skip)
-
-        return self.final_conv(x)
+        return self.maxpool(x)
 
 
-def test():
-    x = torch.randn((1, 95, 512, 512))
-    model = UNET(in_channels=95, out_channels=3)
-    preds = model(x)
-    print(preds.shape)
-    print(x.shape)
+class Up(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=3, stride=2)
+        self.conv = DoubleConvolution(in_channels, out_channels)
+
+    def forward(self, x1, x2):
+        x1 = self.up(x1)
+        # input is CHW
+        diffY = x2.size()[2] - x1.size()[2]
+        diffX = x2.size()[3] - x1.size()[3]
+
+        x1 = F.pad(x1, [diffX // 2, diffX - diffX // 2,
+                        diffY // 2, diffY - diffY // 2])
+        x = torch.cat([x2, x1], dim=1)
+        return self.conv(x)
 
 
-if __name__ == '__main__':
-    test()
-#
-#         self.maxpool_conv = nn.Sequential(nn.MaxPool3d(3),
-#                                           DoubleConvolution(in_channels, out_channels))
-#
-#     def forward(self, x):
-#         return self.maxpool_conv(x)
-#
-#
-# class UpScaling(nn.Module):
-#     def __init__(self, in_channels=3, out_channels=3, ):
-#         super().__init__()
-#         self.up = nn.ConvTranspose3d(self, in_channels, out_channels, 3)
-#         self.conv = DoubleConvolution(in_channels, out_channels)
-#
-#     def forward(self, x):
-#         return self.conv(x)
+class OutConvolution(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super(OutConvolution, self).__init__()
+        self.conv = nn.Conv2d(in_channels, out_channels, kernel_size=1)
+
+    def forward(self, x):
+        return self.conv(x)
