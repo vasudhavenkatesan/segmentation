d5f95e80b65f313116a2e5207a0447680ef1e077
Vasudha Venkatesan <vasudha.govi@gmail.com>
2022-06-12 15:04:12 +0200

    batch size changes in dataloader

--- unet/unet_modules.py
+++ unet/unet_modules.py
@@ -8,11 +8,11 @@ class DoubleConvolution(nn.Module):
         super().__init__()
 
         self.double_conv = nn.Sequential(
-            nn.Conv2d(in_channels, out_channels, kernel_size=2, bias=False),
-            nn.BatchNorm1d(out_channels),
+            nn.Conv2d(in_channels, out_channels, kernel_size=3, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True),
-            nn.Conv2d(out_channels, out_channels, kernel_size=2, bias=False),
-            nn.BatchNorm1d(out_channels),
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, bias=False),
+            nn.BatchNorm2d(out_channels),
             nn.ReLU(inplace=True)
         )
 
@@ -35,7 +35,7 @@ class Down(nn.Module):
 class Up(nn.Module):
     def __init__(self, in_channels, out_channels):
         super().__init__()
-        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=2)
+        self.up = nn.ConvTranspose2d(in_channels, in_channels // 2, kernel_size=3)
         self.conv = DoubleConvolution(in_channels, out_channels)
 
     def forward(self, x1, x2):
